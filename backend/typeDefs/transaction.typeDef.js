b05e7e5d31a5a92a7e7a26a251efb001985ea49a
sahilrawat023 <sahilrawat.sr.rs@gmail.com>
2025-03-11 01:09:48 +0530

    TRANCSACTIONS COMPLETE

--- backend/typeDefs/transaction.typeDef.js
+++ backend/typeDefs/transaction.typeDef.js
@@ -1,23 +1,51 @@
 const transactionTypeDef = `#graphql
+  type Transaction {
+    _id: ID!
+    userId: ID!
+    description: String!
+    paymentType: String!
+    category: String!
+    amount: Float!
+    location: String
+    date: String!
+    user: User!
+  }
 
-type Transaction {
-  _id: ID!
-  userId: ID!
-  description: String!
-  paymentType: String!
-  category: String!
-  amount: Float!
-  location: String
-  date: String!
-}
-type Query {
-  transactions: [Transaction!]
-  transaction(_id: ID!): Transaction
-}
-type Mutation {
-  createTransaction(userId: ID!, description: String!, paymentType: String!, category: String!, amount: Float!, location: String, date: String!): Transaction!
-  updateTransaction(_id: ID!, userId: ID!, description: String!, paymentType: String!, category: String!, amount: Float!, location: String, date: String!): Transaction!
-  deleteTransaction(_id: ID!): Transaction!
-}
+  type Query {
+    transactions: [Transaction!]
+    transaction(transactionId:ID!): Transaction
+    categoryStatistics: [CategoryStatistics!]
+  }
+
+  type Mutation {
+    createTransaction(input: CreateTransactionInput!): Transaction!
+    updateTransaction(input: UpdateTransactionInput!): Transaction!
+    deleteTransaction(transactionId:ID!): Transaction!
+  }
+
+  type CategoryStatistics {
+    category: String!
+    totalAmount: Float!
+  }
+
+  input CreateTransactionInput {
+    description: String!
+    paymentType: String!
+    category: String!
+    amount: Float!
+    date: String!
+    location: String
+  }
+
+  input UpdateTransactionInput {
+    transactionId: ID!
+    description: String
+    paymentType: String
+    category: String
+    amount: Float
+    location: String
+    date: String
+  }
 `;
+
 export default transactionTypeDef;
